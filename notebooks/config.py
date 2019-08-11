fa17025ef935c1521f8c974753150a7bb0531cee
Robert Morse <rob.632@hotmail.co.uk>
2019-08-11 12:15:13 +0100

    abstracted a lot of code into functions

---
+++ notebooks/config.py
@@ -0,0 +1,7 @@
+features_start = 1
+features_end = -2
+target = 'class'
+known_idententifier_col = 'Geology'
+known_identifier_value = 'samples'
+unknown_identifier_value = 'Artefacts'
+random_seed_state = 42
